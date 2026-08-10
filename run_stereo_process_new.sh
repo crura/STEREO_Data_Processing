162e76806d1e2632ec210512718db2403d0f1153
Christopher Rura <85964561+crura@users.noreply.github.com>
2026-08-10 12:00:06 -0400

    try fixing secchi_bkg path

--- run_stereo_process_new.sh
+++ run_stereo_process_new.sh
@@ -9,7 +9,7 @@ git_repo=$(git rev-parse --show-toplevel) &&
 # source $SSW/gen/setup/setup.ssw \loud
 export IDL_DIR="/Applications/harris/idl89" &&
 export secchi="/Volumes/Seagate/Chris/sswdb/secchi" &&
-export SECCHI_BKG="/Users/crura/stereo/secchi/backgrounds" &&
+export SECCHI_BKG="/Volumes/Seagate/Chris/sswdb/secchi/backgrounds" &&
 export SSWDB="/Volumes/Seagate/Chris/sswdb" &&
 export sdb="/Volumes/Seagate/Chris/sswdb" &&
 export VSO_SERVER="http://netdrms02.nispdc.nso.edu/cgi/vsoi_tabdelim" &&
